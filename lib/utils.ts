32b6551064caa74dd81dec255a5e62cfccbf830c
LokeshMehar <meharlokesh395@gmail.com>
2024-12-29 06:26:30 +0530

    format price exported from utils to use]

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[])
+{
   return twMerge(clsx(inputs))
+}
+
+export const formatPrice = (price: number) =>
+{
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  return formatter.format(price)
 }
