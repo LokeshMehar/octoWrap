6bb15a05d0e39f834f5c94dcaced359f3836860e
LokeshMehar <meharlokesh395@gmail.com>
2025-01-19 23:58:02 +0530

    making changes

--- app/configure/design/actions.ts
+++ app/configure/design/actions.ts
@@ -1,15 +1,21 @@
-'use server'
+"use server";
 
-import { db } from '@/db'
-import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from '@prisma/client'
+import { db } from "@/db";
+import
+  {
+    CaseColor,
+    CaseFinish,
+    CaseMaterial,
+    PhoneModel,
+  } from "@prisma/client";
 
 export type SaveConfigArgs = {
-  color: CaseColor
-  finish: CaseFinish
-  material: CaseMaterial
-  model: PhoneModel
-  configId: string
-}
+  color: CaseColor;
+  finish: CaseFinish;
+  material: CaseMaterial;
+  model: PhoneModel;
+  configId: string;
+};
 
 export async function saveConfig({
   color,
@@ -17,9 +23,10 @@ export async function saveConfig({
   material,
   model,
   configId,
-}: SaveConfigArgs) {
+}: SaveConfigArgs)
+{
   await db.configuration.update({
     where: { id: configId },
     data: { color, finish, material, model },
-  })
+  });
 }
