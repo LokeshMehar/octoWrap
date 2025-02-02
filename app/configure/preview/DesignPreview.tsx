844efe50a731821bd14e48998203f3996bb44be6
LokeshMehar <meharlokesh395@gmail.com>
2025-02-02 22:00:49 +0530

    checking the comp which is not working

--- app/configure/preview/DesignPreview.tsx
+++ app/configure/preview/DesignPreview.tsx
@@ -21,6 +21,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const { toast } = useToast();
   const { id } = configuration;
   const { user } = useKindeBrowserClient();
+  console.log(user);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
 
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
