b5dde78f5c9a37d91bd8df8fe3dbbd1c91e3dedd
LokeshMehar <meharlokesh395@gmail.com>
2025-02-08 15:10:05 +0530

    hua

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -57,6 +57,8 @@ export async function POST(req: Request)
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
+      console.log("here in the webhook route");
+
       const updatedOrder = await db.order.update({
         where: {
           id: orderId,
