990bf183420fe7b6a2c9f34b02ff1e47bdbd7cba
LokeshMehar <meharlokesh395@gmail.com>
2025-03-04 19:56:06 +0530

    pta nhi webhook kaam nhi kr rha h

---
+++ app/api/clerk/webhook/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request)
+{
+    try
+    {
+        const clerkEvent: WebhookEvent = await req.json();
+
+        // Ensure it's a user.created event
+        if (clerkEvent.type === "user.created")
+        {
+            const { id, email_addresses } = clerkEvent.data;
+
+            console.log("here in the route of the webhook");
+
+            // Store user in DB
+            await db.user.create({
+                data: {
+                    id,
+                    email: email_addresses[0]?.email_address,
+                },
+            });
+
+            return NextResponse.json({ success: true });
+        }
+
+        return NextResponse.json({ success: false, message: "Event not handled" });
+    } catch (error)
+    {
+        if (error instanceof Error)
+        {
+            return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+        }
+        return NextResponse.json({ success: false, error: "Unknown error" }, { status: 500 });
+    }
+}
