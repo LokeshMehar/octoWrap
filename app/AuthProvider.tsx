9b579ba35f1a3260ed50795f5e29f2dedef22b68
LokeshMehar <meharlokesh395@gmail.com>
2025-05-06 07:19:03 +0530

    changed the style a bit it was not good

--- app/AuthProvider.tsx
+++ app/AuthProvider.tsx
@@ -1,16 +1,12 @@
 "use client";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import { dark, neobrutalism, shadesOfPurple } from '@clerk/themes';
+import { dark, neobrutalism} from '@clerk/themes';
 import { ReactNode } from 'react';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   return <ClerkProvider appearance={{
     baseTheme: [dark, neobrutalism],
-    variables: { colorPrimary: 'orange' },
-    signIn: {
-      baseTheme: [shadesOfPurple],
-      variables: { colorPrimary: 'blue' },
-    },
+    
   }}>{children}</ClerkProvider>;
 };
